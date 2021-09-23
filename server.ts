6640c74fb29c6dd3cff44295274785983ae0c812
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 21:18:56 +0100

    sad

--- server.ts
+++ server.ts
@@ -45,7 +45,7 @@ app.post('/add', async (req,res) => {
   // let breeds = await response.json()
   try {
     
-    console.log(breeds)
+    // console.log(breeds)
     // breeds = breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
     // console.log('Working',breeds)
     // const text = 'INSERT INTO dog (breed,temperament,life_span,weight,height) VALUES($1,$2,$3,$4,$5)';
