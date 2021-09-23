1115812dc414ece24d5e4301f5ac3832c9c6b93b
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 21:11:35 +0100

    sada

--- server.ts
+++ server.ts
@@ -49,7 +49,7 @@ app.post('/add', async (req,res) => {
     breeds = await breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
     console.log('Working',breeds)
     const text = 'INSERT INTO dog (breed,temperament,life_span,weight,height) VALUES($1,$2,$3,$4,$5)';
-    const values = [...breeds];
+    const values = [...breeds[0]];
   
     const resd = await client.query(text, values);
   
