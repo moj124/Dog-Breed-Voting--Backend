996ca2b872cf7622ed9dc23ddf00ac58361f3d7c
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 20:51:03 +0100

    fd

--- server.ts
+++ server.ts
@@ -84,15 +84,15 @@ app.post("/:id", async (req, res) => {
 
 app.post("/add", async (req,res) => {
   try {
-    let response = await fetch( `https://api.thedogapi.com/v1/breeds`);
+    const response = await fetch( `https://api.thedogapi.com/v1/breeds`);
     let breeds = await response.json()
-    // console.log(breeds)
+    console.log(breeds)
     breeds = await breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
     // console.log('Working',breeds)
     const text = 'INSERT INTO dog ("breed","temperament","life_span","weight","height") VALUES($1,$2,$3,$4,$5)';
     const values = breeds;
   
-    await client.query(text, values.join(','));
+    const resd = await client.query(text, values.join(','));
   
       res.status(201).json({
         status: "success"
