a1768fcf8c13bd64add72fb6fa865c5315c018af
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 22:36:00 +0100

    as

--- server.ts
+++ server.ts
@@ -31,12 +31,12 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/leaderboard", async (req, res) => {
-  const dbres = await client.query('select dog.breed as breed, Sum(*) as votes from dog, votes group by dog.breed order by votes desc, dog.breed');
+  const dbres = await client.query('select dog.breed as breed, SUM(votes) as votes from dog, votes group by dog.breed order by votes desc, dog.breed');
   res.json(dbres.rows);
 });
 
 app.get("/top", async (req, res) => {
-  const dbres = await client.query('select dog.breed as breed, Sum(*) as votes from dog, votes group by dog.breed order by votes desc, dog.breed limit 3');
+  const dbres = await client.query('select dog.breed as breed, SUM(votes) as votes from dog, votes group by dog.breed order by votes desc, dog.breed limit 3');
   res.json(dbres.rows);
 });
 
