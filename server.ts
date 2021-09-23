afef706814d174322eccfcf3517a3ae5786e842c
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 20:17:23 +0100

    hg

--- server.ts
+++ server.ts
@@ -88,7 +88,7 @@ app.post("/create", async (req,res) => {
     let breeds = await response.json()
     // console.log(breeds)
     breeds = breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
-    console.log('Working',await breeds)
+    console.log('Working',breeds)
     // const text = "INSERT INTO dog (breed,temperament,life_span,weight,height) VALUES($1,$2,$3,$4,$5)";
     // const values = breeds;
   
