f12b63f8d689f39daa27ca78458d7c2308002f55
michael jones <michael.jowen.jones@gmail.com>
2021-09-23 21:18:48 +0100

    ds

--- server.ts
+++ server.ts
@@ -41,17 +41,17 @@ app.get("/top", async (req, res) => {
 });
 
 app.post('/add', async (req,res) => {
-  const response = await fetch( `https://api.thedogapi.com/v1/breeds`);
-  let breeds = await response.json()
+  // const response = await fetch( `https://api.thedogapi.com/v1/breeds`);
+  // let breeds = await response.json()
   try {
     
     console.log(breeds)
-    breeds = await breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
-    console.log('Working',breeds)
-    const text = 'INSERT INTO dog (breed,temperament,life_span,weight,height) VALUES($1,$2,$3,$4,$5)';
-    const values = [...breeds[0]];
+    // breeds = breeds.map((element: { name: any; temperament: any; life_span: any; weight: { metric: any; }; height: { metric: any; }; }) => `(${element.name},${element.temperament},${element.life_span},${element.weight.metric},${element.height.metric})`);
+    // console.log('Working',breeds)
+    // const text = 'INSERT INTO dog (breed,temperament,life_span,weight,height) VALUES($1,$2,$3,$4,$5)';
+    // const values = [...breeds[0]];
   
-    const resd = await client.query(text, values);
+    // const resd = await client.query(text, values);
   
       res.status(201).json({
         status: "success"
